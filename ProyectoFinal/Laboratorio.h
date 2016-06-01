036b6e7a7fad74815f56cd313eee8e67fbf47920
Roxhn <kevin_jimeneza@hotmail.com>
2016-05-31 22:43:50 -0600

    Create Laboratorio.h

---
+++ ProyectoFinal/Laboratorio.h
@@ -0,0 +1,34 @@
+//LABORATORIO
+
+#ifndef LABORATORIO_H
+#define LABORATORIO_H
+#include "curso.h"
+
+
+class laboratorio : curso
+{
+public:
+    //CONSTRUCTORES Y DESTRUCTORES
+    Laboratorio();
+    Laboratorio(int,char*);
+    virtual ~laboratorio();
+
+    //FUNCIONES Y METODOS DE ENCAPSULAMIENTO(GETTER Y SETTER)
+    void setNumLaboratorio(int);
+    void setInstructor(char*);
+    int getNumLaboratorio() const;
+    const char * getInstructor() const;
+    int getDias() const;
+
+    //FUNCIONES Y METODOS ESPECIALES
+    virtual void toPrint();  //FUNCION QUE IMPRIME EN LA PANTALLA LA INFORMACION DE LA CLASE
+
+private:
+    //DECLARACION DE ATRIBUTOS DE LA CLASE DERIVADA
+    int numLaboratorio;
+    char * instructor;
+    static int dias;
+
+};
+
+#endif // LABORATORIO_H
