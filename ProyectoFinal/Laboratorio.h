601b6d7788677743517367c246deda8dd81a6a0f
Roxhn <kevin_jimeneza@hotmail.com>
2016-06-10 23:16:09 -0600

    Update Laboratorio.h

--- ProyectoFinal/Laboratorio.h
+++ ProyectoFinal/Laboratorio.h
@@ -1,34 +1,24 @@
-//LABORATORIO
-
 #ifndef LABORATORIO_H
 #define LABORATORIO_H
 #include "curso.h"
 
-
-class laboratorio : curso
+class Laboratorio : public Curso
 {
 public:
-    //CONSTRUCTORES Y DESTRUCTORES
     Laboratorio();
-    Laboratorio(int,char);
-    virtual ~laboratorio();
-
-    //FUNCIONES Y METODOS DE ENCAPSULAMIENTO(GETTER Y SETTER)
+    Laboratorio(int,char*,char*,int,char*);
+    virtual ~Laboratorio();
     void setNumLaboratorio(int);
-    void setInstructor(char);
+    void setInstructor(char *);
     int getNumLaboratorio() const;
-    const char getInstructor() const;
+    const char* getInstructor() const;
     int getDias() const;
-
-    //FUNCIONES Y METODOS ESPECIALES
-    virtual void toPrint();  //FUNCION QUE IMPRIME EN LA PANTALLA LA INFORMACION DE LA CLASE
-
+    virtual void toPrint() const;
 private:
-    //DECLARACION DE ATRIBUTOS DE LA CLASE DERIVADA
     int numLaboratorio;
-    char instructor [100];
-    static int dias;
-
+    char * instructor;
+    static const int dias = 6;
 };
 
 #endif // LABORATORIO_H
+
