bacd3bde25323a613c4ce8ad5090f0deb907d0a6
Roxhn <kevin_jimeneza@hotmail.com>
2016-06-10 23:15:03 -0600

    Update Clase.h

--- ProyectoFinal/Clase.h
+++ ProyectoFinal/Clase.h
@@ -1,34 +1,27 @@
-//CLASE
-
 #ifndef CLASE_H
 #define CLASE_H
 #include "curso.h"
 
 
-class clase :/*<LLAMA A LA CLASE BASE>*/ public curso
+class Clase : public Curso
 {
 public:
-    //CONSTRUCTORES Y DESTRUCTORES
     Clase();
-    Clase(int,char,int);
-    virtual ~clase();
-
-    //FUNCIONES Y METODOS DE ENCAPSULAMIENTO(GETTER Y SETTER)
+    Clase(int,char*,char*,int,char*,int);
+    virtual ~Clase();
     void setAula(int);
-    void setCatedratico(char);
+    void setCatedratico(char *);
     void setDias(int);
     int getAula() const;
-    const char getCatedratico() const;
+    const char * getCatedratico() const;
     int getDias() const;
-
-    //FUNCIONES Y METODOS ESPECIALES
-    virtual void toPrint();  //FUNCION QUE IMPRIME EN LA PANTALLA LA INFORMACION DE LA CLASE
+    virtual void toPrint() const;
 
 private:
-    //DECLARACION DE ATRIBUTOS DE LA CLASE DERIVADA
     int aula;
-    char catedratico [100];
+    char * catedratico;
     int dias;
 };
 
 #endif // CLASE_H
+
